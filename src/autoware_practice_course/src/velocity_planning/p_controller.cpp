95b3569f2be966bed1a41226f05cbb63d3c05089
norikenpi <masahiro.kubota@tier4.jp>
2024-04-10 00:45:33 +0900

    feat: Add P controller for velocity planning

---
+++ src/autoware_practice_course/src/velocity_planning/p_controller.cpp
@@ -0,0 +1,48 @@
+#include "p_controller.hpp"
+
+#include <memory>
+
+namespace autoware_practice_course
+{
+
+SampleNode::SampleNode() : Node("sample_node"), target_velocity_(1.0), kp_(0.5)
+{
+  pub_command_ = create_publisher<AckermannControlCommand>("/control/command/control_cmd", rclcpp::QoS(1));
+  
+  velocity_subscriber_ = create_subscription<VelocityReport>(
+    "/simulator/status/velocity", 10, [this](const VelocityReport::SharedPtr msg) {
+      current_velocity_ = msg->longitudinal_velocity; 
+  });
+
+  const auto period = rclcpp::Rate(10).period();
+  timer_ = rclcpp::create_timer(this, get_clock(), period, [this] { on_timer(); });
+}
+
+void SampleNode::on_timer()
+{
+  const auto stamp = now();
+
+  AckermannControlCommand command;
+  command.stamp = stamp;
+  
+  double velocity_error = target_velocity_ - current_velocity_;
+  command.longitudinal.acceleration = kp_ * velocity_error;
+  
+  command.longitudinal.speed = target_velocity_;
+  command.longitudinal.speed = 0.0;
+  
+  command.lateral.steering_tire_angle = 0.0;
+  
+  pub_command_->publish(command);
+}
+
+} 
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<autoware_practice_course::SampleNode>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ src/autoware_practice_course/src/velocity_planning/p_controller.hpp
@@ -0,0 +1,39 @@
+#ifndef P_CONTROLLER_HPP_
+#define P_CONTROLLER_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <autoware_auto_control_msgs/msg/ackermann_control_command.hpp>
+#include <autoware_auto_vehicle_msgs/msg/velocity_report.hpp>
+
+namespace autoware_practice_course
+{
+
+class SampleNode : public rclcpp::Node
+{
+public:
+  SampleNode();
+
+private:
+  using AckermannControlCommand = autoware_auto_control_msgs::msg::AckermannControlCommand;
+  using VelocityReport = autoware_auto_vehicle_msgs::msg::VelocityReport;
+
+  void on_timer();
+
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  rclcpp::Publisher<AckermannControlCommand>::SharedPtr pub_command_;
+
+  rclcpp::Subscription<VelocityReport>::SharedPtr velocity_subscriber_;
+
+  double current_velocity_ = 0.0;
+
+  double target_velocity_ = 0.0;
+
+  double kp_ = 0.0;
+
+  void velocity_callback(const VelocityReport::SharedPtr msg);
+};
+
+} 
+
+#endif
