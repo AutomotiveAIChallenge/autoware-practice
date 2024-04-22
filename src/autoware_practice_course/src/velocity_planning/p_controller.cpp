44ac57ac0b4350da8d0acf77ea658fd4d82b5b69
Masahiro Kubota <42679530+masahiro-kubota@users.noreply.github.com>
2024-04-22 20:33:39 +0900

    feat: add P controller for velocity planning (#17)
    * feat: Add P controller for velocity planning
    
    * fix: change P gain
    
    * fix: change node name
    
    * fix: fix P gain initialization in p_controller.cpp
    
    * fix:  delete unnecessary lines
    
    * fix: To accomodate vehicles of defferent architectures, set the target speed
    
    * fix: change the topic to which the controller subscribes
    
    * fix: To facilitate parameter adjustment, modify to alllow variables to be specifiied from the command line

---
+++ src/autoware_practice_course/src/velocity_planning/p_controller.cpp
@@ -0,0 +1,52 @@
+#include "p_controller.hpp"
+
+#include <memory>
+
+namespace autoware_practice_course
+{
+
+SampleNode::SampleNode() : Node("p_controller")
+{
+  this->declare_parameter<double>("kp", 0.0);
+  this->declare_parameter<double>("target_velocity", 1.0);
+
+  this->get_parameter("kp", kp_);
+  this->get_parameter("target_velocity", target_velocity_);
+
+  pub_command_ = create_publisher<AckermannControlCommand>("/control/command/control_cmd", rclcpp::QoS(1));
+  
+  velocity_subscriber_ = create_subscription<VelocityReport>(
+    "/vehicle/status/velocity_status", 10, [this](const VelocityReport::SharedPtr msg) {
+      current_velocity_ = msg->longitudinal_velocity; 
+  });
+
+  const auto period = rclcpp::Rate(10).period();
+  timer_ = rclcpp::create_timer(this, get_clock(), period, [this] { on_timer(); });
+}
+
+void SampleNode::on_timer()
+{
+  const auto stamp = now();
+
+  AckermannControlCommand command;
+  command.stamp = stamp;
+  
+  double velocity_error = target_velocity_ - current_velocity_;
+  command.longitudinal.acceleration = kp_ * velocity_error;
+  command.longitudinal.speed = target_velocity_;
+  
+  command.lateral.steering_tire_angle = 0.0;
+  
+  pub_command_->publish(command);
+}
+
+} 
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<autoware_practice_course::SampleNode>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
