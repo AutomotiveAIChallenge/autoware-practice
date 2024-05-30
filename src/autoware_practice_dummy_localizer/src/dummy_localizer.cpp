aab0a19d0eabeae2998618d8a46cb0ec4c1ac79e
Masahiro Kubota <norikenpi@gmail.com>
2024-05-30 17:39:49 +0900

    refactor: add namspace and alias declaration

--- src/autoware_practice_dummy_localizer/src/dummy_localizer.cpp
+++ src/autoware_practice_dummy_localizer/src/dummy_localizer.cpp
@@ -1,45 +1,53 @@
 #include "dummy_localizer.hpp"
 
-DummyLocalizer::DummyLocalizer() : Node("dummy_localizer")
+
+
+namespace autoware_practice_course
 {
-    publisher_ = create_publisher<nav_msgs::msg::Odometry>("/localization/kinematic_state", 10);
-    pose_subscriber_ = create_subscription<geometry_msgs::msg::PoseStamped>(
-        "/simulator/ground_truth/pose", 10, std::bind(&DummyLocalizer::pose_callback, this, std::placeholders::_1));
-    twist_subscriber_ = create_subscription<geometry_msgs::msg::TwistStamped>(
-        "/simulator/ground_truth/twist", 10, std::bind(&DummyLocalizer::twist_callback, this, std::placeholders::_1));
+SampleNode::SampleNode() : Node("dummy_localizer"), has_pose_(false), has_twist_(false)
+{
+    publisher_ = create_publisher<Odometry>("/localization/kinematic_state", 10);
+    pose_subscriber_ = create_subscription<PoseStamped>(
+        "/simulator/ground_truth/pose", 10, std::bind(&SampleNode::pose_callback, this, std::placeholders::_1));
+    twist_subscriber_ = create_subscription<TwistStamped>(
+        "/simulator/ground_truth/twist", 10, std::bind(&SampleNode::twist_callback, this, std::placeholders::_1));
 }
 
-void DummyLocalizer::pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg)
+void SampleNode::pose_callback(const PoseStamped::SharedPtr msg)
 {
     last_pose_ = msg->pose;
+    has_pose_ = true;
     publish_odometry();
 }
 
-void DummyLocalizer::twist_callback(const geometry_msgs::msg::TwistStamped::SharedPtr msg)
+void SampleNode::twist_callback(const TwistStamped::SharedPtr msg)
 {
     last_twist_ = msg->twist;
+    has_twist_ = true;
     publish_odometry();
 }
 
-void DummyLocalizer::publish_odometry()
+void SampleNode::publish_odometry()
 {
-    if (last_pose_ && last_twist_) {
+    if (has_pose_ && has_twist_) {
         nav_msgs::msg::Odometry odom;
         odom.header.stamp = now();
         odom.header.frame_id = "odom";
         odom.child_frame_id = "base_link";
-        odom.pose.pose = *last_pose_;
-        odom.twist.twist = *last_twist_;
+        odom.pose.pose = last_pose_;
+        odom.twist.twist = last_twist_;
         publisher_->publish(odom);
-        last_pose_.reset();
-        last_twist_.reset();
+        has_pose_ = false;
+        has_twist_ = false;
     }
+}
+
 }
 
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<DummyLocalizer>();
+    auto node = std::make_shared<autoware_practice_course::SampleNode>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
--- src/autoware_practice_dummy_localizer/src/dummy_localizer.hpp
+++ src/autoware_practice_dummy_localizer/src/dummy_localizer.hpp
@@ -1,26 +1,35 @@
 #ifndef DUMMY_LOCALIZER_HPP
 #define DUMMY_LOCALIZER_HPP
 
-#include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/pose_stamped.hpp"
-#include "geometry_msgs/msg/twist_stamped.hpp"
-#include "nav_msgs/msg/odometry.hpp"
-
-class DummyLocalizer : public rclcpp::Node
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+namespace autoware_practice_course
+{
+class SampleNode : public rclcpp::Node
 {
 public:
-    DummyLocalizer();
+    SampleNode();
 
 private:
-    void pose_callback(const geometry_msgs::msg::PoseStamped::SharedPtr msg);
-    void twist_callback(const geometry_msgs::msg::TwistStamped::SharedPtr msg);
+    using Odometry = nav_msgs::msg::Odometry;
+    using PoseStamped = geometry_msgs::msg::PoseStamped;
+    using TwistStamped = geometry_msgs::msg::TwistStamped;
+    using Pose = geometry_msgs::msg::Pose;
+    using Twist = geometry_msgs::msg::Twist;
+
+    void pose_callback(const PoseStamped::SharedPtr msg);
+    void twist_callback(const TwistStamped::SharedPtr msg);
     void publish_odometry();
 
-    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr publisher_;
-    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_subscriber_;
-    rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr twist_subscriber_;
-    std::optional<geometry_msgs::msg::Pose> last_pose_;
-    std::optional<geometry_msgs::msg::Twist> last_twist_;
+    rclcpp::Publisher<Odometry>::SharedPtr publisher_;
+    rclcpp::Subscription<PoseStamped>::SharedPtr pose_subscriber_;
+    rclcpp::Subscription<TwistStamped>::SharedPtr twist_subscriber_;
+    Pose last_pose_;
+    Twist last_twist_;
+    bool has_pose_;
+    bool has_twist_;
 };
-
+}
 #endif // DUMMY_LOCALIZER_HPP
