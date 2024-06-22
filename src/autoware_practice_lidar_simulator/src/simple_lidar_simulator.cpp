7766f2d5b768e734ab63cf06af3dc94bf65d57a7
Kenta Kato <kenta.kato.4321@gmail.com>
2024-06-22 23:52:18 +0900

    feat: load object csv from share dir (autoware_practice_lidar_simulator)

--- src/autoware_practice_lidar_simulator/src/simple_lidar_simulator.cpp
+++ src/autoware_practice_lidar_simulator/src/simple_lidar_simulator.cpp
@@ -35,8 +35,8 @@ SampleNode::SampleNode() : Node("simple_lidar_simulator")
   timer_ = rclcpp::create_timer(this, get_clock(), period, [this] { on_timer(); });
 
   // 物体の中心位置リストをCSVから読み取って初期化
-  object_centers_ = load_object_centers_from_csv("src/autoware_practice_lidar_simulator/config/object_centers.csv");
-}
+  const auto pkg_path = ament_index_cpp::get_package_share_directory("autoware_practice_lidar_simulator");
+  object_centers_ = load_object_centers_from_csv(pkg_path + "/config/object_centers.csv");}
 
 void SampleNode::pose_callback(const PoseStamped::SharedPtr msg)
 {
