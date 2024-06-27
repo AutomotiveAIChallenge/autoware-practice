5bcbdf5d08b66c27e958c06f0065211c8e6b98a2
Takagi, Isamu <isamu.takagi@tier4.jp>
2024-06-27 18:12:11 +0900

    style: pre-commit
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- src/autoware_practice_lidar_simulator/src/simple_lidar_simulator.cpp
+++ src/autoware_practice_lidar_simulator/src/simple_lidar_simulator.cpp
@@ -14,9 +14,9 @@
 
 #include "simple_lidar_simulator.hpp"
 
-#include <pcl_conversions/pcl_conversions.h>
-
 #include <ament_index_cpp/get_package_share_directory.hpp>
+
+#include <pcl_conversions/pcl_conversions.h>
 
 #include <fstream>
 #include <memory>
@@ -38,7 +38,8 @@ SampleNode::SampleNode() : Node("simple_lidar_simulator")
 
   // 物体の中心位置リストをCSVから読み取って初期化
   const auto pkg_path = ament_index_cpp::get_package_share_directory("autoware_practice_lidar_simulator");
-  object_centers_ = load_object_centers_from_csv(pkg_path + "/config/object_centers.csv");}
+  object_centers_ = load_object_centers_from_csv(pkg_path + "/config/object_centers.csv");
+}
 
 void SampleNode::pose_callback(const PoseStamped::SharedPtr msg)
 {
