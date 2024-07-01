8eabb67bb6c6aede192c7aa246e4d427a7cf2d11
Masahiro Kubota <norikenpi@gmail.com>
2024-07-01 21:52:39 +0900

    feat: create trajectories using bezier interpolate
    Signed-off-by: Masahiro Kubota <norikenpi@gmail.com>

--- src/autoware_practice_course/src/avoidance/trajectory_planner.cpp
+++ src/autoware_practice_course/src/avoidance/trajectory_planner.cpp
@@ -27,12 +27,14 @@ namespace autoware_practice_course
 SampleNode::SampleNode() : Node("trajectory_planner")
 {
   using std::placeholders::_1;
-  GRID_RESOLUTION_ = 3;  // 1セルのサイズ（メートル）
+  GRID_RESOLUTION_ = 1;  // 1セルのサイズ（メートル）
   GRID_WIDTH_ = 100.0;
   GRID_HEIGHT_ = 100.0;
   state_num_ = 10;
 
   pub_trajectory_ = create_publisher<Trajectory>("/planning/scenario_planning/trajectory", rclcpp::QoS(1));
+  pub_trajectory_candidate_ =
+    create_publisher<Trajectory>("/planning/scenario_planning/trajectory_candidate", rclcpp::QoS(1));
   sub_kinematic_state_ = create_subscription<Odometry>(
     "/localization/kinematic_state", rclcpp::QoS(1), std::bind(&SampleNode::update_current_state, this, _1));
   sub_trajectory_ = create_subscription<Trajectory>(
@@ -48,7 +50,7 @@ SampleNode::SampleNode() : Node("trajectory_planner")
 
 void SampleNode::update_current_state(const Odometry & msg)  // called by sub_kinematic_state_
 {
-  RCLCPP_INFO(this->get_logger(), "Updating current state.");
+  // RCLCPP_INFO(this->get_logger(), "Updating current state.");
   current_velocity_ = msg.twist.twist.linear.x;
   current_position_ = msg.pose.pose.position;
   current_orientation_ = msg.pose.pose.orientation;
@@ -57,7 +59,7 @@ void SampleNode::update_current_state(const Odometry & msg)  // called by sub_ki
 
 void SampleNode::update_reference_trajectory(const Trajectory & msg)  // called by sub_trajectory_
 {
-  RCLCPP_INFO(this->get_logger(), "Updating reference trajectory.");
+  // RCLCPP_INFO(this->get_logger(), "Updating reference trajectory.");
   reference_trajectory_ = msg;
   reference_trajectory_initialized_ = true;
 };
@@ -66,7 +68,7 @@ void SampleNode::update_pointcloud(const PointCloud2 & msg)  // called by sub_po
 {
   pointcloud_ = msg;
   pointcloud_initialized_ = true;
-  RCLCPP_INFO(this->get_logger(), "PointCloud received and updated.");
+  // RCLCPP_INFO(this->get_logger(), "PointCloud received and updated.");
 };
 
 void SampleNode::on_timer()
@@ -74,42 +76,47 @@ void SampleNode::on_timer()
   if (!current_state_initialized_ || !reference_trajectory_initialized_ || !pointcloud_initialized_) {
     RCLCPP_WARN(this->get_logger(), "Waiting for all initial data to be received.");
   } else {
-    RCLCPP_INFO(this->get_logger(), "Timer triggered, creating trajectory.");
-    Trajectory trajectory = create_trajectory();
-    pub_trajectory_->publish(trajectory);
+    // RCLCPP_INFO(this->get_logger(), "Timer triggered, creating trajectory.");
+    create_trajectory();
+    pub_trajectory_->publish(best_trajectory_);
+    pub_trajectory_candidate_->publish(trajectory_candidate_);
   }
 }
 
-SampleNode::Trajectory SampleNode::create_trajectory()  // called by on_timer()
+void SampleNode::create_trajectory()  // called by on_timer()
 {
-  RCLCPP_INFO(this->get_logger(), "Creating trajectory.");
-  // state lattice planner
-  // create trajectory library
+  // RCLCPP_INFO(this->get_logger(), "Creating trajectory.");
+  //  state lattice planner
+  //  create trajectory library
   std::vector<Trajectory> trajectory_set = create_trajectory_set();
+
+  trajectory_candidate_ = trajectory_set[0];
 
   // create costmap
   std::vector<std::vector<float>> costmap = create_costmap();
 
   // evaluate trahectories by the cost map
-  Trajectory best_trajectory = evaluate_trajectory(trajectory_set, costmap);
-  RCLCPP_INFO(this->get_logger(), "Logging trajectory with %zu points", best_trajectory.points.size());
+  best_trajectory_ = evaluate_trajectory(trajectory_set, costmap);
+  // RCLCPP_INFO(this->get_logger(), "Logging trajectory with %zu points", best_trajectory.points.size());
+  /*
   for (size_t i = 0; i < best_trajectory.points.size(); ++i) {
     const auto & point = best_trajectory.points[i];
     RCLCPP_INFO(
       this->get_logger(), "Point %zu: x=%f, y=%f, z=%f", i, point.pose.position.x, point.pose.position.y,
       point.pose.position.z);
-  }
 
-  return best_trajectory;
+  }
+  */
 }
 
 std::vector<SampleNode::Trajectory> SampleNode::create_trajectory_set()
 {
-  RCLCPP_INFO(this->get_logger(), "Creating trajectory set.");
+  // RCLCPP_INFO(this->get_logger(), "Creating trajectory set.");
 
   // 目標状態集合を計算
   std::vector<TrajectoryPoint> target_trajectory_point_set = create_target_state_set();
-  RCLCPP_INFO(this->get_logger(), "Target trajectory point set created. Size: %zu", target_trajectory_point_set.size());
+  // RCLCPP_INFO(this->get_logger(), "Target trajectory point set created. Size: %zu",
+  // target_trajectory_point_set.size());
 
   if (target_trajectory_point_set.size() < 2) {
     RCLCPP_ERROR(this->get_logger(), "Target trajectory point set size is less than 2.");
@@ -124,35 +131,41 @@ std::vector<SampleNode::Trajectory> SampleNode::create_trajectory_set()
   current_q.y() = current_orientation_.y;
   current_q.z() = current_orientation_.z;
   current_q.w() = current_orientation_.w;
-  RCLCPP_INFO(
+  /*RCLCPP_INFO(
     this->get_logger(), "Current orientation set: x=%f, y=%f, z=%f, w=%f", current_orientation_.x,
     current_orientation_.y, current_orientation_.z, current_orientation_.w);
-
+  */
   target_q.x() = target_trajectory_point_set[1].pose.orientation.x;
   target_q.y() = target_trajectory_point_set[1].pose.orientation.y;
   target_q.z() = target_trajectory_point_set[1].pose.orientation.z;
   target_q.w() = target_trajectory_point_set[1].pose.orientation.w;
-  RCLCPP_INFO(
+  /*RCLCPP_INFO(
     this->get_logger(), "Target orientation set: x=%f, y=%f, z=%f, w=%f", target_q.x(), target_q.y(), target_q.z(),
     target_q.w());
-
-  double current_inclination = quaternionToInclination(current_q);
-  double target_inclination = quaternionToInclination(target_q);
-  RCLCPP_INFO(
+  */
+  // double current_inclination = quaternionToInclination(current_q);
+  // double target_inclination = quaternionToInclination(target_q);
+  Eigen::Vector3d current_vector = quaternionToVector(current_q);
+  Eigen::Vector3d target_vector = quaternionToVector(target_q);
+  /*RCLCPP_INFO(
     this->get_logger(), "Current inclination: %f, Target inclination: %f", current_inclination, target_inclination);
+  */
 
   // 車両の位置姿勢と目標状態集合をエルミート補間し、軌道を生成
+  trajectory_candidate_.points.clear();
   for (const auto & target_trajectory_point : target_trajectory_point_set) {
+    /*
     RCLCPP_INFO(
       this->get_logger(), "Processing target trajectory point at position: (%f, %f, %f)",
       target_trajectory_point.pose.position.x, target_trajectory_point.pose.position.y,
       target_trajectory_point.pose.position.z);
+    */
 
-    double num_points = 10;
+    double num_points = 20;
 
     // 車両の位置姿勢と目標状態をエルミート補間
     std::vector<Point> interpolated_points = hermiteInterpolate(
-      current_position_, target_trajectory_point.pose.position, current_inclination, target_inclination, num_points);
+      current_position_, target_trajectory_point.pose.position, current_vector, target_vector, num_points);
 
     Trajectory trajectory_candidate;
 
@@ -162,31 +175,36 @@ std::vector<SampleNode::Trajectory> SampleNode::create_trajectory_set()
       trajectory_point.pose.position = interpolated_point;
       trajectory_point.longitudinal_velocity_mps = 2.0;
       trajectory_candidate.points.push_back(trajectory_point);
-
-      RCLCPP_INFO(
-        this->get_logger(), "Added interpolated point to trajectory candidate. Point position: (%f, %f, %f)",
-        interpolated_point.x, interpolated_point.y, interpolated_point.z);
+      trajectory_candidate_.points.push_back(trajectory_point);
+      /*
+            RCLCPP_INFO(
+              this->get_logger(), "Added interpolated point to trajectory candidate. Point position: (%f, %f, %f)",
+              interpolated_point.x, interpolated_point.y, interpolated_point.z);
+      */
     }
 
-    RCLCPP_INFO(
-      this->get_logger(), "Trajectory candidate created. Number of points: %zu", trajectory_candidate.points.size());
-
+    /*
+        RCLCPP_INFO(
+          this->get_logger(), "Trajectory candidate created. Number of points: %zu",
+       trajectory_candidate.points.size());
+    */
     // 補間された曲線状のTrajectory Pointを生成
     trajectory_set.push_back(trajectory_candidate);
-
-    RCLCPP_INFO(
-      this->get_logger(), "Added trajectory candidate to trajectory set. Current trajectory set size: %zu",
-      trajectory_set.size());
+    /*
+        RCLCPP_INFO(
+          this->get_logger(), "Added trajectory candidate to trajectory set. Current trajectory set size: %zu",
+          trajectory_set.size());
+    */
   }
 
-  RCLCPP_INFO(this->get_logger(), "Trajectory set created. Size: %zu", trajectory_set.size());
+  // RCLCPP_INFO(this->get_logger(), "Trajectory set created. Size: %zu", trajectory_set.size());
 
   return trajectory_set;
 }
 
 std::vector<autoware_auto_planning_msgs::msg::TrajectoryPoint> SampleNode::create_target_state_set()
 {
-  RCLCPP_INFO(this->get_logger(), "Creating target state set.");
+  // RCLCPP_INFO(this->get_logger(), "Creating target state set.");
 
   if (reference_trajectory_.points.empty()) {
     RCLCPP_ERROR(this->get_logger(), "Reference trajectory is empty.");
@@ -195,9 +213,11 @@ std::vector<autoware_auto_planning_msgs::msg::TrajectoryPoint> SampleNode::creat
     // 車両に最も近いtrajectory pointを取得
     double min_distance = std::numeric_limits<double>::max();
     size_t closest_waypoint_index = 0;
+    /*
     RCLCPP_INFO(
       this->get_logger(), "Creating target state set. Number of points in reference trajectory: %zu",
       reference_trajectory_.points.size());
+    */
 
     for (size_t i = 0; i < reference_trajectory_.points.size(); ++i) {
       double dx = reference_trajectory_.points[i].pose.position.x - current_position_.x;
@@ -211,10 +231,11 @@ std::vector<autoware_auto_planning_msgs::msg::TrajectoryPoint> SampleNode::creat
       }
     }
 
-    RCLCPP_INFO(this->get_logger(), "Closest waypoint index: %zu, Distance: %f", closest_waypoint_index, min_distance);
+    // RCLCPP_INFO(this->get_logger(), "Closest waypoint index: %zu, Distance: %f", closest_waypoint_index,
+    // min_distance);
 
     int target_trajectory_point_index = closest_waypoint_index + 10;
-    RCLCPP_INFO(this->get_logger(), "Target trajectory point index: %d", target_trajectory_point_index);
+    // RCLCPP_INFO(this->get_logger(), "Target trajectory point index: %d", target_trajectory_point_index);
 
     // 10個先のtrajectory pointを取得
     autoware_auto_planning_msgs::msg::TrajectoryPoint target_trajectory_point =
@@ -227,11 +248,11 @@ std::vector<autoware_auto_planning_msgs::msg::TrajectoryPoint> SampleNode::creat
     target_trajectory_point.pose.orientation.y = q.y();
     target_trajectory_point.pose.orientation.z = q.z();
     target_trajectory_point.pose.orientation.w = q.w();
-
-    RCLCPP_INFO(
-      this->get_logger(), "Target trajectory point position: (%f, %f, %f)", target_trajectory_point.pose.position.x,
-      target_trajectory_point.pose.position.y, target_trajectory_point.pose.position.z);
-
+    /*
+        RCLCPP_INFO(
+          this->get_logger(), "Target trajectory point position: (%f, %f, %f)", target_trajectory_point.pose.position.x,
+          target_trajectory_point.pose.position.y, target_trajectory_point.pose.position.z);
+    */
     // 目標状態集合を生成
     // target_trajectory_pointの姿勢に直交する方向に並ぶstate_num個の状態を生成
     // クォータニオンを回転行列に変換
@@ -240,7 +261,7 @@ std::vector<autoware_auto_planning_msgs::msg::TrajectoryPoint> SampleNode::creat
     // 直交するベクトルの選択（第二列ベクトル）
     Eigen::Vector3d v = R.col(1);
 
-    RCLCPP_INFO(this->get_logger(), "Orthogonal vector: (%f, %f, %f)", v.x(), v.y(), v.z());
+    // RCLCPP_INFO(this->get_logger(), "Orthogonal vector: (%f, %f, %f)", v.x(), v.y(), v.z());
 
     // 点の配置
     double d = 1.0;  // 点と点の間の距離
@@ -251,10 +272,11 @@ std::vector<autoware_auto_planning_msgs::msg::TrajectoryPoint> SampleNode::creat
       target_state.pose.position.y += n * d * v.y();
       target_state.pose.position.z += n * d * v.z();
       target_state_set.push_back(target_state);
-
-      RCLCPP_INFO(
-        this->get_logger(), "Added target state: index %d, position (%f, %f, %f)", n, target_state.pose.position.x,
-        target_state.pose.position.y, target_state.pose.position.z);
+      /*
+            RCLCPP_INFO(
+              this->get_logger(), "Added target state: index %d, position (%f, %f, %f)", n,
+         target_state.pose.position.x, target_state.pose.position.y, target_state.pose.position.z);
+              */
     }
 
     return target_state_set;
@@ -307,7 +329,7 @@ Eigen::Quaterniond SampleNode::vectorToQuaternion(const Eigen::Vector3d & start,
 
 std::vector<std::vector<float>> SampleNode::create_costmap()
 {
-  RCLCPP_INFO(this->get_logger(), "Creating costmap.");
+  // RCLCPP_INFO(this->get_logger(), "Creating costmap.");
   pcl::PointCloud<pcl::PointXYZ>::Ptr pointcloud_pcl(new pcl::PointCloud<pcl::PointXYZ>());
   // 変換関数を呼び出し
   pcl::fromROSMsg(pointcloud_, *pointcloud_pcl);
@@ -351,20 +373,20 @@ std::vector<std::vector<float>> SampleNode::create_costmap()
 SampleNode::Trajectory SampleNode::evaluate_trajectory(
   const std::vector<SampleNode::Trajectory> & trajectory_set, const std::vector<std::vector<float>> & costmap)
 {
-  RCLCPP_INFO(this->get_logger(), "Evaluating trajectories.");
+  // RCLCPP_INFO((this->get_logger(), "Evaluating trajectories.");
   Trajectory best_trajectory;
   std::vector<float> trajectory_cost(trajectory_set.size(), 0.0f);
   size_t index = 0;
 
   // trajectory_setをcostmapで評価し、最適な軌道を選択
   for (const auto & trajectory_candidate : trajectory_set) {
-    RCLCPP_INFO(this->get_logger(), "Evaluating trajectory %zu", index);
+    // RCLCPP_INFO(this->get_logger(), "Evaluating trajectory %zu", index);
 
     // trajectoryをcostmapで評価
     for (const auto & trajectory_point : trajectory_candidate.points) {
       int x_index = static_cast<int>(trajectory_point.pose.position.x / GRID_RESOLUTION_);
       int y_index = static_cast<int>((trajectory_point.pose.position.y + GRID_RESOLUTION_ / 2) / GRID_RESOLUTION_);
-      RCLCPP_INFO(this->get_logger(), "x_index=%d, y_index=%d", x_index, y_index);
+      // RCLCPP_INFO(this->get_logger(), "x_index=%d, y_index=%d", x_index, y_index);
 
       if (x_index >= 0 && x_index < GRID_WIDTH_ && y_index >= 0 && y_index < GRID_HEIGHT_) {
         trajectory_cost[index] += costmap[x_index][y_index];
@@ -372,13 +394,14 @@ SampleNode::Trajectory SampleNode::evaluate_trajectory(
         RCLCPP_WARN(
           this->get_logger(), "Trajectory point out of costmap bounds: x_index=%d, y_index=%d", x_index, y_index);
       }
-
-      RCLCPP_INFO(
-        this->get_logger(), "Trajectory point (%f, %f) evaluated with cost %f", trajectory_point.pose.position.x,
-        trajectory_point.pose.position.y, costmap[x_index][y_index]);
+      /*
+            RCLCPP_INFO(
+              this->get_logger(), "Trajectory point (%f, %f) evaluated with cost %f", trajectory_point.pose.position.x,
+              trajectory_point.pose.position.y, costmap[x_index][y_index]);
+              */
     }
 
-    RCLCPP_INFO(this->get_logger(), "Total cost for trajectory %zu: %f", index, trajectory_cost[index]);
+    // RCLCPP_INFO(this->get_logger(), "Total cost for trajectory %zu: %f", index, trajectory_cost[index]);
     index++;
   }
 
@@ -387,7 +410,8 @@ SampleNode::Trajectory SampleNode::evaluate_trajectory(
     std::distance(trajectory_cost.begin(), std::min_element(trajectory_cost.begin(), trajectory_cost.end()));
   best_trajectory = trajectory_set[min_index];
 
-  RCLCPP_INFO(this->get_logger(), "Best trajectory selected: %d with cost %f", min_index, trajectory_cost[min_index]);
+  // RCLCPP_INFO(this->get_logger(), "Best trajectory selected: %d with cost %f", min_index,
+  // trajectory_cost[min_index]);
 
   return best_trajectory;
 }
@@ -398,34 +422,44 @@ double SampleNode::quaternionToInclination(Eigen::Quaterniond q)
   return inclination;
 }
 
+Eigen::Vector3d SampleNode::quaternionToVector(Eigen::Quaterniond q)
+{
+  Eigen::Vector3d unitVector(1, 0, 0);
+  Eigen::Vector3d directionVector = q * unitVector;
+  // 計算された方向ベクトルをログに記録
+  RCLCPP_WARN(
+    this->get_logger(), "Direction Vector: [%f, %f, %f]", directionVector.x(), directionVector.y(),
+    directionVector.z());
+  return directionVector;
+}
 // エルミート補間関数
 std::vector<geometry_msgs::msg::Point> SampleNode::hermiteInterpolate(
-  const geometry_msgs::msg::Point & p0, const geometry_msgs::msg::Point & p1, double m0, double m1, int numPoints)
+  const geometry_msgs::msg::Point & p0, const geometry_msgs::msg::Point & p1, Eigen::Vector3d m0, Eigen::Vector3d m1,
+  int numPoints)
 {
-  RCLCPP_INFO(this->get_logger(), "Performing Hermite interpolation.");
+  // RCLCPP_INFO(this->get_logger(), "Performing Hermite interpolation.");
   std::vector<geometry_msgs::msg::Point> interpolatedPoints;
 
   Eigen::Vector3d v0 = pointToVector3d(p0);
   Eigen::Vector3d v1 = pointToVector3d(p1);
 
-  for (int i = 0; i < numPoints; ++i) {
-    double t = static_cast<double>(i) / (numPoints - 1);
+  // Control points
+  Eigen::Vector3d c0 = v0;
+  Eigen::Vector3d c1 = v0 + m0 * 3;
+  Eigen::Vector3d c2 = v1 - m1 * 3;
+  Eigen::Vector3d c3 = v1;
+
+  for (int i = 0; i <= numPoints; ++i) {
+    double t = static_cast<double>(i) / numPoints;
     double t2 = t * t;
     double t3 = t2 * t;
+    double one_minus_t = 1.0 - t;
+    double one_minus_t2 = one_minus_t * one_minus_t;
+    double one_minus_t3 = one_minus_t2 * one_minus_t;
 
-    double h00 = 2 * t3 - 3 * t2 + 1;
-    double h10 = t3 - 2 * t2 + t;
-    double h01 = -2 * t3 + 3 * t2;
-    double h11 = t3 - t2;
+    Eigen::Vector3d point = one_minus_t3 * c0 + 3.0 * one_minus_t2 * t * c1 + 3.0 * one_minus_t * t2 * c2 + t3 * c3;
 
-    Eigen::Vector3d interpolatedVector = h00 * v0 + h10 * m0 * (v1 - v0) + h01 * v1 + h11 * m1 * (v1 - v0);
-
-    geometry_msgs::msg::Point point;
-    point.x = interpolatedVector.x();
-    point.y = interpolatedVector.y();
-    point.z = interpolatedVector.z();
-
-    interpolatedPoints.push_back(point);
+    interpolatedPoints.push_back(vector3dToPoint(point));
   }
 
   return interpolatedPoints;
--- src/autoware_practice_course/src/avoidance/trajectory_planner.hpp
+++ src/autoware_practice_course/src/avoidance/trajectory_planner.hpp
@@ -51,21 +51,24 @@ private:
   void update_current_state(const Odometry & msg);
   void update_reference_trajectory(const Trajectory & msg);
   void update_pointcloud(const PointCloud2 & msg);
-  Trajectory create_trajectory();
+  void create_trajectory();
   std::vector<Trajectory> create_trajectory_set();
   TrajectoryPoint calculate_target_trajectory_point();
   std::vector<std::vector<float>> create_costmap();
   Trajectory evaluate_trajectory(
     const std::vector<Trajectory> & trajectory_set, const std::vector<std::vector<float>> & costmap);
   std::vector<TrajectoryPoint> create_target_state_set();
-  std::vector<Point> hermiteInterpolate(const Point & p0, const Point & p1, double m0, double m1, int numPoints);
+  std::vector<Point> hermiteInterpolate(
+    const Point & p0, const Point & p1, Eigen::Vector3d m0, Eigen::Vector3d m1, int numPoints);
   double quaternionToInclination(Eigen::Quaterniond q);
+  Eigen::Vector3d quaternionToVector(Eigen::Quaterniond q);
   Eigen::Vector3d pointToVector3d(const geometry_msgs::msg::Point & point);
   geometry_msgs::msg::Point vector3dToPoint(const Eigen::Vector3d & vector);
   Eigen::Quaterniond vectorToQuaternion(const Eigen::Vector3d & start, const Eigen::Vector3d & end);
 
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<Trajectory>::SharedPtr pub_trajectory_;
+  rclcpp::Publisher<Trajectory>::SharedPtr pub_trajectory_candidate_;
   rclcpp::Subscription<Odometry>::SharedPtr sub_kinematic_state_;
   rclcpp::Subscription<Trajectory>::SharedPtr sub_trajectory_;
   rclcpp::Subscription<PointCloud2>::SharedPtr sub_pointcloud_;
@@ -78,6 +81,8 @@ private:
   Quaternion current_orientation_;
   double current_velocity_;
   Trajectory reference_trajectory_;
+  Trajectory best_trajectory_;
+  Trajectory trajectory_candidate_;
   int state_num_;
 
   bool current_state_initialized_ = false;
